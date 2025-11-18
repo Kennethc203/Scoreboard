8fe896c153f117fed445ba4e0bb26b233f4fb0db
Kennethc203 <kennethc203@nycstudents.net>
2025-11-18 15:27:36 -0500

    It's fixed. Pls work

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,61 @@ import java.io.File;
 import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        String[] colors = new String[7];
-        colors[0] = "Orange";
-        colors[1] = "Yellow";
-        colors[2] = "Red";
-        colors[3] = "Indigo";
-        colors[4] = "Blue";
-        colors[5] = "Violet";
-        colors[6] = "Green";
-        int[] wins = new int[7];
+        System.out.println("ExclamationPoint");
+        Scoreboard game = new Scoreboard("Red", "Blue");
+        System.out.println(game.getScore());
+        game.recordPlay(1);
+        System.out.println(game.getScore());
+        game.recordPlay(0);
+        System.out.println(game.getScore());
+        game.recordPlay(3);
+        System.out.println(game.getScore());
+        game.recordPlay(1);
+        game.recordPlay(0);
+        System.out.println(game.getScore());
+        game.recordPlay(0);
+        game.recordPlay(4);
+        game.recordPlay(0);
+        String info = game.getScore();
+
+        game = new Scoreboard("Lions", "Tigers");
+        System.out.println(game.getScore());
+        System.out.println(info);
+
+
+
+        //Time for the task again
+        String[] teams = new String[7];
+        teams[0] = "Orange";
+        teams[1] = "Yellow";
+        teams[2] = "Red";
+        teams[3] = "Green";
+        teams[4] = "Blue";
+        teams[5] = "Indigo";
+        teams[6] = "Violet";
+        int[] teamsWins = new int[7];
+        String team1 = "";
+        String team2 = "";
+        Scoreboard match;
+        int winner = 0;
+        String winnerName = "";
         File f = new File("Scoreboard.txt");
         Scanner s = new Scanner(f);
-        Team team1;
-        Team team2;
-        Team winner;
-        Scoreboard scoreboard;
-        for (int i = 0; i < 1000; i++){
-            team1 = new Team(s.next());
-            team2 = new Team(s.next());
-            scoreboard = new Scoreboard(team1, team2);
-            while (s.hasNextInt()){
-                scoreboard.play(s.nextInt());
-            }
-            winner = scoreboard.winner();
-            for (int j = 0; j < colors.length; j++){
-                if (colors[j].equals(winner.getName())){
-                    wins[j]++;
-                    System.out.println(winner.getName());
+        while (s.hasNext()){
+            team1 = s.next();
+            team2 = s.next();
+            match = new Scoreboard(team1, team2);
+            while (s.hasNextInt()) match.recordPlay(s.nextInt());
+            winnerName = match.returnWinner();
+            winner = match.returnWinnerPoints();
+            for (int i = 0; i < teams.length; i++){
+                if (winnerName.equals(teams[i])) {
+                    teamsWins[i] += 1;
                 }
             }
         }
-        System.out.println();
-        for (int i = 0; i < colors.length; i++){
-            System.out.println(" " + colors[i] + " " + wins[i]);
+        for (int i = 0; i < teams.length; i++){
+            System.out.println(teams[i] + " "+ teamsWins[i]);
         }
     }
 }
--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -1,34 +1,39 @@
 public class Scoreboard {
-    private Team team1;
-    private Team team2;
-    private Team active;
+    private String team1;
+    private String team2;
+    private int team1Points = 0;
+    private int team2Points = 0;
+    private String activeTeam;
 
-    public Scoreboard(String name1, String name2) {
-        team1 = new Team(name1);
-        team2 = new Team(name2);
-        active = team1;
-    }
-    public Scoreboard(Team team1, Team team2){
+    public Scoreboard(String team1, String team2) {
         this.team1 = team1;
         this.team2 = team2;
-        this.active = this.team1;
+        activeTeam = team1;
     }
-    public void play(int points){
-        active.addPoints(points);
-        if (points == 0){
-            if (active.equals(team1)) active = team2;
-            else active = team2;
-        }
-    }
-    public Team winner(){
-        Team winner = new Team("");
-        if (team1.getPoints() > team2.getPoints()){
-            winner = team1;
-        }
-        else if (team2.getPoints() > team1.getPoints()){
-            winner = team2;
-        }
-        return winner;
 
+    public void recordPlay(int points) {
+        if (activeTeam.equals(team1)) {
+            if (points == 0) activeTeam = team2;
+            team1Points += points;
+        } else {
+            if (points == 0) activeTeam = team1;
+            team2Points += points;
+        }
     }
+
+    public String getScore() {
+        return team1Points + "-" + team2Points + "-" + activeTeam;
+    }
+
+    public String returnWinner(){
+        if (team1Points > team2Points) return team1;
+        else if (team2Points > team1Points) return team2;
+        return "";
+    }
+    public int returnWinnerPoints(){
+        if (returnWinner().equals(team1)) return team1Points;
+        else if (returnWinner().equals(team2)) return team2Points;
+        return 0;
+    }
+
 }
